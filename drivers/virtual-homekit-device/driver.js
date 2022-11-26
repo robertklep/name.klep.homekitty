b913895fb9a0d1726fd17aa62e1f9c9fb00c9e66
Robert Klep <robert@klep.name>
2022-11-26 11:16:01 +0100

    Started implementing Set Characteristic Value flow action card

--- drivers/virtual-homekit-device/driver.js
+++ drivers/virtual-homekit-device/driver.js
@@ -4,6 +4,34 @@ const { Service } = require('../../modules/hap-nodejs');
 module.exports = class VirtualHomeKitDriver extends Driver {
   async onInit() {
     this.log('VirtualHomeKitDriver has been initialized');
+
+    this.cards = {
+      setCharacteristicCard : this.setupSetCharacteristicCard()
+    }
+  }
+
+  setupSetCharacteristicCard() {
+    return this.homey.flow
+      .getActionCard('set-characteristic')
+      .registerArgumentAutocompleteListener('name', async (query, args) => {
+        // retrieve the device's service
+        const serviceName = args.device?.getSetting('HomeKit.Service');
+        if (! serviceName) throw Error('no service name?');
+
+        // instantiate it so we can find its characteristics
+        const service = new Service[serviceName]();
+        const characteristics = [
+          ...service.characteristics,
+          ...service.optionalCharacteristics,
+        ].map(characteristic => ({ name : characteristic.constructor?.name || characteristic.name }));
+
+        // match characteristics against query
+        return characteristics.filter(characteristic => characteristic.name?.toLowerCase().includes(query.toLowerCase()));
+      }).registerRunListener(async (args, state) => {
+        console.log('Run listener');
+        console.log('A', args);
+        console.log('S', state);
+      });
   }
 
   onPair(session) {
